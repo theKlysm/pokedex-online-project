fa7794d335b8d1f6e50f088100e20f54ce098f92
theKlysm <130249836+theKlysm@users.noreply.github.com>
2023-05-17 00:00:05 -0300

    Add body class dark-mode and themeChangeButtonImage

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,10 @@ const themeChangeButton = document.getElementById("theme-change-button"); //pass
 
 const body = document.querySelector("body"); //pass-2
 
+const themeChangeButtonImage = document.querySelector(".button-image");
+
 themeChangeButton.addEventListener("click", () => {
-    
+    body.classList.add("dark-mode");//pass-4
+    themeChangeButtonImage,setAttribute("src", "./")
+
 });//pass-3
