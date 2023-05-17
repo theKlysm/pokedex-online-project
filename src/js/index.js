ca4cf87d68a7f4cb5b22a1bf17269bec091e1fdb
theKlysm <130249836+theKlysm@users.noreply.github.com>
2023-05-17 00:22:33 -0300

    Add themeChangeButtonImage const to change the img when the user click in theme, and change the button name id

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
-const themeChangeButton = document.getElementById("theme-change-button"); //pass-1
+const themeChangeButton = document.getElementById("change-theme-button"); //pass-1
 
 const body = document.querySelector("body"); //pass-2
 
-const themeChangeButtonImage = document.querySelector(".button-image");
+const themeChangeButtonImage = document.querySelector(".button-image");//pass-5
 
 themeChangeButton.addEventListener("click", () => {
     body.classList.add("dark-mode");//pass-4
-    themeChangeButtonImage,setAttribute("src", "./")
 
+    themeChangeButtonImage.setAttribute("src", "./src/images/moon.png");//pass-5
 });//pass-3
