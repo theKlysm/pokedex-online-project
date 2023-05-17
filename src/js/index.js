6d9706f512fa81125e06952b3eb90f4c02c099dd
theKlysm <130249836+theKlysm@users.noreply.github.com>
2023-05-17 00:24:02 -0300

    Add activeDarkMode var with statement

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,14 @@ const body = document.querySelector("body"); //pass-2
 
 const themeChangeButtonImage = document.querySelector(".button-image");//pass-5
 
+const activeDarkMode = body.classList.contains("dark-mode");
+
 themeChangeButton.addEventListener("click", () => {
     body.classList.add("dark-mode");//pass-4
 
     themeChangeButtonImage.setAttribute("src", "./src/images/moon.png");//pass-5
+
+    if(activeDarkMode){
+        //console.log("dark-mode esta ativo");
+    };//pass-6
 });//pass-3
