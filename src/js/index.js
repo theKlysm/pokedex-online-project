34b33fe83f3ee1585005160371e6a2ef1b73e58f
theKlysm <130249836+theKlysm@users.noreply.github.com>
2023-05-16 23:48:50 -0300

    Add themeChangeButton and body consts and click event listener

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
-console.log(document.getElementById("theme-change-button"))
+const themeChangeButton = document.getElementById("theme-change-button"); //pass-1
+
+const body = document.querySelector("body"); //pass-2
+
+themeChangeButton.addEventListener("click", () => {
+    
+});//pass-3
