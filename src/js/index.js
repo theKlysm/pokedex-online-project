cbd90fd4fe73d0a45c08e4f73b2b2bee529675bf
theKlysm <130249836+theKlysm@users.noreply.github.com>
2023-05-17 23:06:25 -0300

    Change some var names to make the code more simple

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,20 @@
-const themeChangeButton = document.getElementById("change-theme-button"); //pass-1
-const body = document.querySelector("body"); //pass-2
-const themeChangeButtonImage = document.querySelector(".button-image"); //pass-5
+const changeThemeButton = document.getElementById("change-theme-button");
+const body = document.querySelector("body");
+const changeThemeButtonImage = document.querySelector(".button-image");
 
-
-themeChangeButton.addEventListener("click", () => {
+changeThemeButton.addEventListener("click", () => {
     const activeDarkMode = body.classList.contains("dark-mode");
     
-    body.classList.toggle("dark-mode"); //alternate the value of the class. Can substitute .add and .remove
+    body.classList.toggle("dark-mode");
+    //alternate the value of the class. Can substitute .add and .remove
 
     if(activeDarkMode){
         //console.log("dark-mode esta ativo");
         //body.classList.remove("dark-mode");
-        themeChangeButtonImage.setAttribute("src", "./src/images/sun.png"); //pass-8
+        changeThemeButtonImage.setAttribute("src", "./src/images/sun.png");
     } else {
-        //body.classList.add("dark-mode");//pass-4
+        //body.classList.add("dark-mode");
 
-        themeChangeButtonImage.setAttribute("src", "./src/images/moon.png"); //pass-5
+        changeThemeButtonImage.setAttribute("src", "./src/images/moon.png");
     }
-}); //pass-3
+});
