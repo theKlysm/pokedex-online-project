5c37aebbdc5e9a4bc8fcf13f6c97d06cb49e86ca
theKlysm <130249836+theKlysm@users.noreply.github.com>
2023-05-17 22:56:50 -0300

    Add some spaces to the code to solve some errors

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
-const themeChangeButton = document.getElementById("change-theme-button");//pass-1
+const themeChangeButton = document.getElementById("change-theme-button"); //pass-1
 const body = document.querySelector("body"); //pass-2
-const themeChangeButtonImage = document.querySelector(".button-image");//pass-5
+const themeChangeButtonImage = document.querySelector(".button-image"); //pass-5
 
 
 themeChangeButton.addEventListener("click", () => {
     const activeDarkMode = body.classList.contains("dark-mode");
     
-    body.classList.toogle("dark-mode");//alternate the value of the class. Can substitute .add and .remove
+    body.classList.toogle("dark-mode"); //alternate the value of the class. Can substitute .add and .remove
 
     if(activeDarkMode){
         //console.log("dark-mode esta ativo");
@@ -15,6 +15,6 @@ themeChangeButton.addEventListener("click", () => {
     } else {
         //body.classList.add("dark-mode");//pass-4
 
-        themeChangeButtonImage.setAttribute("src", "./src/images/moon.png");//pass-5
+        themeChangeButtonImage.setAttribute("src", "./src/images/moon.png"); //pass-5
     }
-});//pass-3
+}); //pass-3
