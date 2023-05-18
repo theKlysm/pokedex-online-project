8813bbd23af880aae11abddab0471e7d50cf2c0e
theKlysm <130249836+theKlysm@users.noreply.github.com>
2023-05-17 22:35:39 -0300

    Add toogle and substitute add and remove

--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,18 @@ const body = document.querySelector("body"); //pass-2
 
 const themeChangeButtonImage = document.querySelector(".button-image");//pass-5
 
+
 themeChangeButton.addEventListener("click", () => {
     const activeDarkMode = body.classList.contains("dark-mode");
     
+    body.classList.toogle("dark-mode");//alternate the value of the class. Can substitute .add and .remove
+
     if(activeDarkMode){
         //console.log("dark-mode esta ativo");
-        body.classList.remove("dark-mode");
+        //body.classList.remove("dark-mode");
         themeChangeButtonImage.setAttribute("src", "./src/images/sun.png"); //pass-8
     } else {
-        body.classList.add("dark-mode");//pass-4
+        //body.classList.add("dark-mode");//pass-4
 
         themeChangeButtonImage.setAttribute("src", "./src/images/moon.png");//pass-5
     }
